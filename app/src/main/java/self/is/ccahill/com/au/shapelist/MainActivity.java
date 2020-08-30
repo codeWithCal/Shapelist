39ecfbc09bdd5e18c143ddb2b94222e057e96cfb
Callum Hill <ccahill.is@gmail.com>
2020-08-30 15:12:00 +1000

    Shapelist with Search

--- app/src/main/java/self/is/ccahill/com/au/shapelist/MainActivity.java
+++ app/src/main/java/self/is/ccahill/com/au/shapelist/MainActivity.java
@@ -6,7 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.SearchView;
 
 import java.util.ArrayList;
 
@@ -17,15 +19,61 @@ public class MainActivity extends AppCompatActivity
 
     private ListView listView;
 
+    private String selectedFilter = "all";
+    private String currentSearchText = "";
+    private SearchView searchView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initSearchWidgets();
         setupData();
         setUpList();
         setUpOnclickListener();
+    }
+
+    private void initSearchWidgets()
+    {
+        searchView = (SearchView) findViewById(R.id.shapeListSearchView);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s)
+            {
+                currentSearchText = s;
+                ArrayList<Shape> filteredShapes = new ArrayList<Shape>();
+
+                for(Shape shape: shapeList)
+                {
+                    if(shape.getName().toLowerCase().contains(s.toLowerCase()))
+                    {
+                        if(selectedFilter.equals("all"))
+                        {
+                            filteredShapes.add(shape);
+                        }
+                        else
+                            {
+                                if(shape.getName().toLowerCase().contains(selectedFilter))
+                                {
+                                    filteredShapes.add(shape);
+                                }
+                            }
+                    }
+                }
+                ShapeAdapter adapter = new ShapeAdapter(getApplicationContext(), 0, filteredShapes);
+                listView.setAdapter(adapter);
+
+                return false;
+            }
+        });
     }
 
     private void setupData()
@@ -84,4 +132,69 @@ public class MainActivity extends AppCompatActivity
 
     }
 
+
+
+    private void filterList(String status)
+    {
+        selectedFilter = status;
+
+        ArrayList<Shape> filteredShapes = new ArrayList<Shape>();
+
+        for(Shape shape: shapeList)
+        {
+            if(shape.getName().toLowerCase().contains(status))
+            {
+                if(currentSearchText == "")
+                {
+                    filteredShapes.add(shape);
+                }
+                else
+                    {
+                        if(shape.getName().toLowerCase().contains(currentSearchText.toLowerCase()))
+                        {
+                            filteredShapes.add(shape);
+                        }
+                    }
+            }
+        }
+
+        ShapeAdapter adapter = new ShapeAdapter(getApplicationContext(), 0, filteredShapes);
+        listView.setAdapter(adapter);
+    }
+
+
+
+
+    public void allFilterTapped(View view)
+    {
+        selectedFilter = "all";
+
+        ShapeAdapter adapter = new ShapeAdapter(getApplicationContext(), 0, shapeList);
+        listView.setAdapter(adapter);
+    }
+
+    public void triangleFilterTapped(View view)
+    {
+        filterList("triangle");
+    }
+
+    public void squareFilterTapped(View view)
+    {
+        filterList("square");
+    }
+
+    public void octagonFilterTapped(View view)
+    {
+        filterList("octagon");
+    }
+
+    public void rectangleFilterTapped(View view)
+    {
+        filterList("rectangle");
+    }
+
+    public void circleFilterTapped(View view)
+    {
+        filterList("circle");
+    }
 }
